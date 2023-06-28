1765960868044f62adae6311d237ccd330a6e025
Prem Chavhan <104148610+premchavhan99@users.noreply.github.com>
2023-06-28 13:21:20 +0530

    Update build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build -f 17/alpine/hotspot/Dockerfile . --tag elestio4test/jenkins:latest
+docker build --build-arg RELEASE_LINE=war -f 17/alpine/hotspot/Dockerfile . --tag elestio4test/jenkins:latest

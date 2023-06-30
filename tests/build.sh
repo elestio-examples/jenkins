3d44fd7d45abf934773a33f66895a1a7958077e2
Prem Chavhan <pchavhan@logiquad.com>
2023-06-30 16:16:08 +0530

    Updated build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 sed -i "s~-jdk-alpine~-jdk-centos7~g" ./17/alpine/hotspot/Dockerfile
 sed -i "s~ARG RELEASE_LINE~ARG RELEASE_LINE=war~g" ./17/alpine/hotspot/Dockerfile
-docker build-f 17/alpine/hotspot/Dockerfile . --tag elestio4test/jenkins:latest
+docker build -f 17/alpine/hotspot/Dockerfile . --tag elestio4test/jenkins:latest

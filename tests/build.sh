29ecc9ccf445d966ccd2950340d7283a380ab0f7
Prem Chavhan <pchavhan@logiquad.com>
2023-07-03 19:17:31 +0530

    Updated build.sh

--- tests/build.sh
+++ tests/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sed -i "s~-https://get.jenkins.io/~-https://get.jenkins.io/war~g" ./17/rhel/ubi9/hotspot/Dockerfile
-docker build -f /17/rhel/ubi9/hotspot/Dockerfile . --tag elestio4test/jenkins:latest
+docker build -f ./17/rhel/ubi9/hotspot/Dockerfile . --tag elestio4test/jenkins:latest
